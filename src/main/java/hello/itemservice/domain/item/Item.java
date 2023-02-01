193e61ce378e9a3b8bd9a1d4066e89ffb41fd63d
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2023-02-01 15:00:52 +0900

    fefactor: 오탈자 수정

--- src/main/java/hello/itemservice/domain/item/Item.java
+++ src/main/java/hello/itemservice/domain/item/Item.java
@@ -15,7 +15,7 @@ public class Item {
     private Boolean open; //판매 여부
     private List<String> regions; //등록 지역
     private ItemType itemType; //상품 종류
-    private String deliverCode; //배송 방식
+    private String deliveryCode; //배송 방식
 
     public Item() {
     }
