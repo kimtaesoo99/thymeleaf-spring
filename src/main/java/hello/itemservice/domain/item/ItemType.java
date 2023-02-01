87f74d90e0405ee9d9d5ae53966bb6715b503847
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2023-02-01 14:52:23 +0900

    feat: 게터 추가

--- src/main/java/hello/itemservice/domain/item/ItemType.java
+++ src/main/java/hello/itemservice/domain/item/ItemType.java
@@ -9,4 +9,8 @@ public enum ItemType {
     ItemType(String description) {
         this.description = description;
     }
+
+    public String getDescription() {
+        return description;
+    }
 }
